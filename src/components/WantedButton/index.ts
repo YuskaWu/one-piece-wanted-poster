ded9a8f3895f8253024b7c94c18b936ce90e5789
Yuska <yuska@msn.com>
2022-03-27 18:36:46 +0800

    implement wanted-button component

---
+++ src/components/WantedButton/index.ts
@@ -0,0 +1,95 @@
+import cssContent from './style.css?inline'
+import ButtonImageUrl from './wanted.png'
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wanted-button': WantedButton
+  }
+}
+
+const ATTRIBUTES = ['icon', 'text', 'font-size', 'loading'] as const
+
+type Attributes = typeof ATTRIBUTES
+export type WantedPosterAttribute = Partial<{
+  [key in Attributes[number]]: string
+}>
+
+const template = document.createElement('template')
+template.innerHTML = `
+  <img class="img" src="${ButtonImageUrl}"/>
+  <div class="icon">
+    <i class="fa"></i>
+  </div>
+  <span class="text"></span>
+`
+//
+class WantedButton extends HTMLElement {
+  #icon: string = ''
+  #iconElm: HTMLElement
+  #textElm: HTMLSpanElement
+
+  constructor() {
+    super()
+
+    // Create a shadow root
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+
+    const style = document.createElement('style')
+    style.textContent = cssContent
+
+    // attach the created elements to the shadow DOM
+    shadowRoot.append(style, template.content.cloneNode(true))
+
+    this.#iconElm = shadowRoot.querySelector('i.fa') as HTMLElement
+    this.#textElm = shadowRoot.querySelector('span.text') as HTMLSpanElement
+
+    // Since Web components are completely isolated entities, so parent or document style will
+    // not be available inside a web component, that's why we clone font-awesome link element here
+    // to make font-awesome work inside out custom button
+    const fontawesomeLink = document.querySelector('link[href*="font-awesome"]')
+    if (fontawesomeLink) {
+      shadowRoot.appendChild(fontawesomeLink.cloneNode())
+    }
+  }
+
+  static get observedAttributes(): Attributes {
+    return ATTRIBUTES
+  }
+
+  attributeChangedCallback(
+    attributeName: Attributes[number],
+    _: string,
+    newValue: string
+  ) {
+    switch (attributeName) {
+      case 'icon':
+        this.#icon = newValue
+        this.#iconElm.classList.add(newValue)
+        break
+
+      case 'text':
+        this.#textElm.textContent = newValue
+        break
+
+      case 'font-size':
+        this.#textElm.style.fontSize = newValue
+        break
+
+      case 'loading':
+        if (newValue !== null) {
+          this.style.pointerEvents = 'none'
+          this.#iconElm.classList.remove(this.#icon)
+          this.#iconElm.classList.add('fa-circle-notch', 'fa-spin')
+        } else {
+          this.style.pointerEvents = 'auto'
+          this.#iconElm.classList.remove('fa-circle-notch', 'fa-spin')
+          this.#iconElm.classList.add(this.#icon)
+        }
+        break
+    }
+  }
+}
+
+customElements.define('wanted-button', WantedButton)
+
+export default WantedButton
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import './style.css'
+import './components/WantedButton'
 import './components/WantedPoster'
 import './components/App'
