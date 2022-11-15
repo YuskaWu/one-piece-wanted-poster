a073aaa5300ca0a5e489859c8410a736dd001c27
Yuska <yuska@msn.com>
2022-11-15 08:13:04 +0800

    fix: fix export error when avatar-url is cross-origin

--- src/components/WantedPoster/utils.ts
+++ src/components/WantedPoster/utils.ts
@@ -1,6 +1,7 @@
 export async function loadImage(url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image()
+    img.setAttribute('crossorigin', 'anonymous')
     img.onload = () => resolve(img)
     img.onerror = reject
     img.src = url
