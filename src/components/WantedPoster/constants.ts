ebd8f6e08f4fea5de36bf91252cce12f43b48c2d
Yuska <yuska@msn.com>
2022-11-26 05:36:13 +0800

    fix: adjust bounty & name position

--- src/components/WantedPoster/constants.ts
+++ src/components/WantedPoster/constants.ts
@@ -1,16 +1,16 @@
-import { WantedImageInfo } from './types'
 import onePieceWantedImageUrl from './images/one-piece-wanted.png'
+import { WantedImageInfo } from './types'
 
 export const ONE_PIECE_WANTED_IMAGE: WantedImageInfo = {
   url: onePieceWantedImageUrl,
   width: 772,
   height: 1154,
   avatarPosition: { x: 74, y: 252, width: 638, height: 484 },
-  namePosition: { x: 88, y: 826, width: 585, height: 114 },
+  namePosition: { x: 87, y: 826, width: 585, height: 114 },
   bountyPosition: {
-    x: 88,
+    x: 84,
     y: 958,
-    width: 586,
+    width: 584,
     height: 85
   },
   bountyFontSize: 70,
