019bb6b99c758d0330751c51945611ce37ede694
Yuska <yuska@msn.com>
2023-04-06 20:32:16 +0800

    fix: fix broken lanuch handler

--- src/components/App/launch-handler.ts
+++ src/components/App/launch-handler.ts
@@ -20,18 +20,9 @@ declare global {
   }
 }
 
-const isSupported =
-  window.launchQueue && window.LaunchParams && window.LaunchParams.files
-
 function setConsumer(
   callback: (fileHandles: NonNullable<LaunchParams['files']>) => void
 ) {
-  if (!isSupported) {
-    console.warn(
-      '[Launch Handler API] browser does not support Launch Handler API, ignoring consumption.'
-    )
-    return
-  }
   window.launchQueue?.setConsumer((launchParams) => {
     // Nothing to do when the queue is empty.
     if (!launchParams.files?.length) {
@@ -48,4 +39,4 @@ function isFileHandle(
   return fileHandle.kind === 'file'
 }
 
-export default { setConsumer, isSupported, isFileHandle }
+export default { setConsumer, isFileHandle }
