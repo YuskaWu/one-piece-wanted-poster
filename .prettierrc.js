d703def1304f3fe7f98e59e67210701a78dd5d0a
Yuska <yuska@msn.com>
2023-09-27 06:03:49 +0800

    chore: add stylelint

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-const standardConfig = require('prettier-config-standard');
-module.exports = {
-  ...standardConfig,
-  semi: false,
-  singleQuote: true,
-  plugins: ['prettier-plugin-css-order']
-};
