0d379363b904aeee3aca38683129585d82ca7db9
Yuska <yuska@msn.com>
2023-10-12 21:31:35 +0800

    fix: getter and setter of custom element not work

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import 'iconify-icon'
-import './components/App'
-import './components/EditPanel'
-import './components/IconButton'
+// The import order should start from nested custom element so that parent component
+// can recognize it during creation(invoking constructor), otherwise nested custom element
+// will be treated as normal HTMLElement and the setter and getter defined inside custom element
+// will not work
+// https://stackoverflow.com/a/73870508
 import './components/LuffyLogo'
-import './components/OptcGalleryPanel'
-import './components/TipsDialog'
+import './components/FontSelect'
 import './components/WantedButton'
+import './components/IconButton'
+import './components/TipsDialog'
 import './components/WantedPoster'
+import './components/EditPanel'
+import './components/OptcGalleryPanel'
+import './components/App'
 import './style.css'
